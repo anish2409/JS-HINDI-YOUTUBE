dfd845a24c0e0eec517bfd7d3e9de51fc040302a
anish2409 <139654946+anish2409@users.noreply.github.com>
2023-09-25 02:29:53 +0000

    datatype summery

---
+++ 01_basics/05_Datatypes_summery.js
@@ -0,0 +1,40 @@
+// primitive 
+
+//7 types :String ,Number,Boolean ,null,undefiend,bigint
+
+
+//Reference(Non primitive)
+
+//array,objects,functions
+
+const score = 100
+
+const isLoggedIn = false
+const  outsideTemp = null
+let userEmail ;
+const id = Symbol('123')
+const anotherId= Symbol('123')
+
+//console.log(id === anotherId);
+
+
+const bigNumber = 58812257412685225555n
+
+//Reference (Non primitive)
+
+
+//Array , Objects, Functions
+
+const heros =[ "shaktiman","naagraj","doga"]
+
+let myObj = {
+    name:"hitesh",
+    age:22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction)
