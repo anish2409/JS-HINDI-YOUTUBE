76b1a60e67834b1f3fe2771a93beaac0673f4fe4
anish2409 <139654946+anish2409@users.noreply.github.com>
2023-09-25 00:56:27 +0000

    Datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail="ani7895@gmail.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+let accountstate
+
+//accountId = 2
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs,not browser
+
+console.log()//code readability should be high
+
+let name = "Anish"
+let age = 9
+let isLoggedIn = false
+
+
+//number
+//bigint
+//string
+//boolean   
+//null
+// undefined
+//symbol
+
+
+//object
+
+console.log(typeof "Anish");
