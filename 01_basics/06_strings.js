4324f3445d7482066379f6e22b2287f623310578
anish2409 <139654946+anish2409@users.noreply.github.com>
2023-09-27 03:43:40 +0000

    strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,33 @@
+const name="Anish"
+const repoCount = 50
+
+//console.log(name + repoCount+ "value")
+
+console.log(`Hello my name ${name} and my repo count is ${repoCount}` );
+
+const gameName = new String ('anish-hc-com')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0,4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-6,2)
+//console.log(anotherString);
+
+const newStringOne = "  anish  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('hitesh'))
+
+console.log(gameName.split('-'))
