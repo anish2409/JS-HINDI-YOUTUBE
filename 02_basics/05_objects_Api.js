69e00129c61de66c705f4ef352ce8aaa739023ac
anish2409 <139654946+anish2409@users.noreply.github.com>
2023-09-30 16:27:26 +0000

    object by rest api

---
+++ 02_basics/05_objects_Api.js
@@ -0,0 +1,32 @@
+
+const course = {
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructure : "hitesh"
+
+}
+//course.courseInstructure
+
+const {courseInstructure :CI}= course
+console.log(CI);
+//Destructureing object  in react
+/*const  navbar = ({course}) => {
+
+}
+
+navbar (company = "hitesh")*/
+//JSON
+// {
+//     "name" : "hitesh",
+//     "coursename" : "js in hindi",
+//     "price" : "free"
+
+
+// }
+[
+    {},
+    {
+
+    },
+    {}
+]
